5618162017ba988f796b6958d3e623cf7f28dcc8
XtremeOrnob <xtreme.ornob@gmail.com>
2021-10-06 16:47:52 +0000

    sdm845-common: Nuking LiveDisplay for now
    Signed-off-by: XtremeOrnob <xtreme.ornob@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -271,8 +271,8 @@ PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sdm845
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+# PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
