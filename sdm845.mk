b7c355a6ff178031bcfe5e8a71554e4a0c28c56d
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-10-23 10:43:09 +0000

    sdm845-common: Enable freeform windows
    Signed-off-by: XtremeOrnob <xtreme.ornob@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -31,6 +31,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
