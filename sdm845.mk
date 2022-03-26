2f4c3ecb46cc2711b65b88fd35f9922cf5c975d9
XtremeOrnob <xtreme.ornob@gmail.com>
2022-03-26 06:29:47 +0000

    sdm845-common: Build android.hardware.drm 1.4
     * and nuke drm clearkey 1.3 in favor of 1.4
    
     * Fixes Chrome crashes
    
    Signed-off-by: XtremeOrnob <xtreme.ornob@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -249,7 +249,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
