81f650db7a9fd52374a46a98e196259c76069faf
jjpprrrr <zhongcy95@gmail.com>
2022-04-03 16:45:28 +0000

    sdm845-common: build libdisplayconfig.system.qti for QCOM WFD
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: XtremeOrnob <xtreme.ornob@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -246,6 +246,7 @@ PRODUCT_PACKAGES += \
     gralloc.sdm845 \
     hwcomposer.sdm845 \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     libqdMetaData \
     libtinyxml \
     libtinyxml.vendor \
